16175fe7175c62ea820d561ccb8f16fcc028e9b3
jem <michael.jerger@meissa-gmbh.de>
2020-05-08 14:13:28 +0200

    separate provision targets

---
+++ infrastrucure/05-jitsi-hetzner-instance/jitsi.tf
@@ -0,0 +1,34 @@
+resource "hcloud_server" "jitsi" {
+  name        = "${var.module}.${var.stage}.meissa"
+  image       = "ubuntu-18.04"
+  server_type = "cx31"
+  ssh_keys    = [hcloud_ssh_key.jitsi_ssh_key.id]
+}
+
+resource "aws_route53_record" "v4" {
+  zone_id = data.terraform_remote_state.aws_dns.outputs.dda_test_zone_id
+  name    = "${var.module}"
+  type    = "A"
+  ttl     = "300"
+  records = [hcloud_server.jitsi.ipv4_address]
+}
+
+resource "aws_route53_record" "v6" {
+  zone_id = data.terraform_remote_state.aws_dns.outputs.dda_test_zone_id
+  name    = "${var.module}-neu"
+  type    = "AAAA"
+  ttl     = "300"
+  records = ["${hcloud_server.jitsi.ipv6_address}1"]
+}
+
+output "ipv4" {
+  value = hcloud_server.jitsi.ipv4_address
+}
+
+output "ipv6" {
+  value = hcloud_server.jitsi.ipv6_address
+}
+
+output "fqdn" {
+  value = aws_route53_record.v4.fqdn
+}
---
+++ infrastrucure/05-jitsi-hetzner-instance/with_jem_integration.sh
@@ -0,0 +1,3 @@
+STAGE=integration \
+    HETZNER_API_KEY_PATH='jem/meissa/hetzner/jem_test' \
+    $@
