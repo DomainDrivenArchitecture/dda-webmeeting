4f8d668f84e973fc4cc4e34a8abf63f198ccb5f7
Tobias Frech <tobias@frech.info>
2020-05-08 18:56:43 +0200

    key und IP-Ausgabe

--- infrastrucure/05-jitsi-aws-instance/jitsi.tf
+++ infrastrucure/05-jitsi-aws-instance/jitsi.tf
@@ -18,7 +18,11 @@ resource "aws_instance" "web" {
   ami           = "${data.aws_ami.ubuntu.id}"
   instance_type = "t3.large"
 
+  key_name = "${aws_key_pair.deployer.key_name}"
+
   tags = {
     Name = "HelloWorld"
   }
 }
+
+output "ipv4" { value = aws_instance.web.public_ip }
